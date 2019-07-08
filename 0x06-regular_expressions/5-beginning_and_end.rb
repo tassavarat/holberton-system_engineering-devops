685a93f3e6fbf74dc8449253f41ac90622092a9e
tassavarat <tassavarat@gmail.com>
2019-07-08 13:25:35 -0700

    Updated comments

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
-# Regular expression to match repetitions
+# Regular expression to match a string that starts with h and ends with n and
+# can have any single character between
 
 puts ARGV[0].scan(/^h.n$/).join

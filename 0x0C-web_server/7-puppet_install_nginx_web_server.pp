d89846a11f5532aae92272442554ffb226d8b398
tassavarat <tassavarat@gmail.com>
2019-08-08 15:11:44 -0700

    Formatting

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,8 +9,8 @@ file { '/var/www/html/index.html':
 }
 
 fine_line { 'Add redirection, 301':
-  ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
+  ensure => 'present',
   after  => 'listen 80 default_server',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }

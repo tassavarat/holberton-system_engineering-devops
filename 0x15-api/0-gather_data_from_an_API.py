891e8a033ba3d0a43f24a359e32f42012f0c0403
Tim Assavarat <tassavarat@gmail.com>
2019-09-17 00:30:43 +0000

    Stopped execution on import

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """Uses given employee ID to return imformation"""
 
-import requests
-from sys import argv
-import urllib
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    import urllib
 
-NUMBER_OF_DONE_TASKS = 0
-TASK_TITLE = []
-r = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                 format(argv[1])).json()
-EMPLOYEE_NAME = r.get("name")
-r = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                 format(argv[1])).json()
-for i in r:
-    if i.get("completed") is True:
-        TASK_TITLE.append(i.get("title"))
-        NUMBER_OF_DONE_TASKS += 1
-TOTAL_NUMBER_OF_TASKS = len(r)
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = []
+    r = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                     format(argv[1])).json()
+    EMPLOYEE_NAME = r.get("name")
+    r = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                     format(argv[1])).json()
+    for i in r:
+        if i.get("completed") is True:
+            TASK_TITLE.append(i.get("title"))
+            NUMBER_OF_DONE_TASKS += 1
+    TOTAL_NUMBER_OF_TASKS = len(r)
 
-print("Employee {} is done with tasks({}/{}):".
-      format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-for title in TASK_TITLE:
-    print("\t {}".format(title))
+    print("Employee {} is done with tasks({}/{}):".
+          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    for title in TASK_TITLE:
+        print("\t {}".format(title))

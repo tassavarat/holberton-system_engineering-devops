49f001ad79a01918dfed39fd14855167bc8dd499
Tim Assavarat <tassavarat@gmail.com>
2019-09-17 04:31:36 +0000

    First commit

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Exports all tasks in JSON format"""
+
+if __name__ == "__main__":
+    import json
+    import requests
+
+    all_dict = {}
+    r1 = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    r2 = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    all_dict = {j.get("id"): [{"task": i.get("title"),
+                               "completed": i.get("completed"),
+                               "username": j.get("username")} for i in r2]
+                for j in r1}
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(all_dict, jsonfile)

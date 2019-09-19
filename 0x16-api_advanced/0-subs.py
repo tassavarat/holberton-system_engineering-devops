47f1b6a8d597659c8bd699f45262bedff3304df7
Tim Assavarat <tassavarat@gmail.com>
2019-09-19 21:20:25 +0000

    Fixed documentation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Queries Reddit API
-Return: Number of subcribers for given subreddit
-0 if invalid subreddit
-"""
+"""Queries Reddit API"""
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """Return: Number of subcribers for given subreddit
+    0 if invalid subreddit
+    """
     r = requests.get("https://reddit.com/r/{}/about.json".format(subreddit),
                      headers={"User-agent": "custom"})
     if(r.status_code == 200):

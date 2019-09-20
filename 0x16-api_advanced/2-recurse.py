c2118056f1283273df66eceb7c80462e9ba2b94a
Tim Assavarat <tassavarat@gmail.com>
2019-09-20 02:58:28 +0000

    Rewrote after assignment

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -12,12 +12,29 @@ def recurse(subreddit, hot_list=[], after=""):
                      headers={"User-agent": "custom"})
 
     if r.status_code != 200:
+        # print("status code not 200")
         return None
-    after = r.json().get("data").get("after")
-    if not r.json().get("data").get("after"):
+    after = r.json().get("data").get("children")[0].get("data").get("name")
+    # print("after is", after)
+    if not r.json().get("data").get("children")[0].get("data").get("name"):
+        # print("in base case")
         return hot_list
 
     hot_list.append(r.json().get("data").get("children")[0].get("data").
                     get("title"))
+    # print("recursing")
     recurse(subreddit, hot_list, after)
+    # print("hot_list is", hot_list)
     return hot_list
+
+
+'''
+    while r.json().get("data").get("children")[0].get("data").get("after"):
+        r = requests.get("https://reddit.com/r/{}/hot.json?after={}".
+                         format(subreddit, r.json().get("data").get("children")[0].get("data").get("name")), headers={"User-agent": "custom"})
+        hot_list.append(r.json().get("data").get("children")[0].get("data").get("title"))
+        print(hot_list)
+    print("####")
+    print(r.json().get("data").get("children")[2].get("data").get("title"))
+    print("####")
+'''

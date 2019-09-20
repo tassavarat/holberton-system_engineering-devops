7c21f65add940a107ba5cb2ea483c6180cd1e5af
Tim Assavarat <tassavarat@gmail.com>
2019-09-20 02:05:32 +0000

    First commit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""2-recurse module"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """Return: List containing titles of all hot articles for given subreddit
+    None if no results found
+    """
+    r = requests.get("https://reddit.com/r/{}/hot.json?after={}".
+                     format(subreddit, after),
+                     headers={"User-agent": "custom"})
+    after = r.json().get("data").get("after")
+
+    if not r.json().get("data").get("after"):
+        return hot_list
+
+    hot_list.append(r.json().get("data").get("children")[0].get("data").
+                    get("title"))
+    recurse(subreddit, hot_list, after)

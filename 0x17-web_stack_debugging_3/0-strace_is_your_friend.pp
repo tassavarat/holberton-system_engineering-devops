958d8d1d6810dda2bbd466bab512caa4e5e7e2a2
Tim Assavarat <tassavarat@gmail.com>
2019-10-02 02:57:57 +0000

    First commit

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Handles 500 apache error
+
+exec { '/var/www/html/wp-settings.php':
+  command => "sed -i 's/\\/class-wp-locale.phpp/\\/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+  path    => '/bin',
+}

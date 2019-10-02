4f21b377ba2230829f68491f629d6227ae468151
Tim Assavarat <tassavarat@gmail.com>
2019-10-02 03:07:35 +0000

    Linter compliance

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # Handles 500 apache error
 
 exec { '/var/www/html/wp-settings.php':
-  command => "sed -i 's/\\/class-wp-locale.phpp/\\/class-wp-locale.php/g' /var/www/html/wp-settings.php",
-  path    => '/bin',
+  command => "sed -i 's/\\/class-wp-locale.phpp/\\/class-wp-locale.php/g'\
+	      /var/www/html/wp-settings.php",
+  path    => '/bin/',
 }
